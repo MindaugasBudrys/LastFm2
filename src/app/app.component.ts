ae40a21cfaea8c00e2ed5cc1d1e17213d35da869
Mindaugas <budr.mindaugas@gmail.com>
2017-08-03 15:04:01 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { PostService } from './post.service';
+import { PostComponent } from './post/post.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [PostService]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app works!';
 }
